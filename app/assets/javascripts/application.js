642c93311ed52cac52f4b375185bd70f86075953
ProgrammingSam <programmingsam@gmail.com>
2018-12-19 17:24:18 +0100

    Add boostrap, jquery and add locales (french only)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,16 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require bootstrap4/alert
+//= require bootstrap4/button
+//= require bootstrap4/carousel
+//= require bootstrap4/collapse
+//= require bootstrap4/dropdown
+//= require bootstrap4/index
+//= require bootstrap4/modal
+//= require bootstrap4/popover
+//= require bootstrap4/scrollspy
+//= require bootstrap4/tab
+//= require bootstrap4/tooltip
+//= require bootstrap4/util
 //= require_tree .
