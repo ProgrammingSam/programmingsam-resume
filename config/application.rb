60124335414622bd37805a8d3829ccb057fca296
ProgrammingSam <programmingsam@gmail.com>
2019-01-05 18:02:38 +0100

    Remove some useless dependencie and files

--- config/application.rb
+++ config/application.rb
@@ -13,13 +13,13 @@ require_relative 'boot'
 require 'active_model/railtie'
 # And now the rest
 require 'action_controller/railtie'
-require 'action_mailer/railtie'
+# require 'action_mailer/railtie'
 require 'action_view/railtie'
-require 'active_job/railtie'
+# require 'active_job/railtie'
 require 'action_cable/engine'
 require 'active_storage/engine'
 require 'sprockets/railtie'
-require 'rails/test_unit/railtie'
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
