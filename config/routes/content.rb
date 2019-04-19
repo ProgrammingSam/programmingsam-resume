9f297888adc7c6bea13ad2bba31b2ffc4f17237d
ProgrammingSam <programmingsam@gmail.com>
2019-04-19 19:44:16 +0200

    Add routes and refactor

---
+++ config/routes/content.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+resources :about, only: :index
+resources :curriculum_vitae, only: :index
+resources :skills, only: :index
+resources :team, only: :index
+resources :tools, only: :index
